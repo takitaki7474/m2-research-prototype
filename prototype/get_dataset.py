8efc06cfe97ea85392d809c51a1316be5bbe64f1
oota <shark747474@gmail.com>
2020-03-30 23:29:04 +0900

    add get_specific_label_dataset()

--- prototype/get_dataset.py
+++ prototype/get_dataset.py
@@ -20,3 +20,13 @@ def get_one_label_dataset(class_label, train_dataset, test_dataset):
     train = get_one_label_data(class_label, train_dataset)
     test = get_one_label_data(class_label, test_dataset)
     return train, test
+
+# 特定クラスラベルリストのtrain, testデータセットを取得
+def get_specific_label_dataset(class_label_list, train_dataset, test_dataset):
+    new_train = []
+    new_test = []
+    for label in class_label_list:
+        train, test = get_one_label_dataset(label, train_dataset, test_dataset)
+        new_train += train
+        new_test += test
+    return new_train, new_test
