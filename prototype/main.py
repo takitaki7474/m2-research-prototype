5e407f2eb6e449611fbf2bf58b629ef2823c2de0
oota <shark747474@gmail.com>
2020-04-01 04:41:09 +0900

    update main

--- prototype/main.py
+++ prototype/main.py
@@ -6,7 +6,7 @@ import log
 import sys
 
 # 学習対象のネットワークを指定
-net = chainer_cifar10.Cifar10()
+net = chainer_cifar10.Cifar10_Conv2()
 # 使用するデータのクラスを指定
 class_label_list = [1,2,7]
 
@@ -30,7 +30,7 @@ if __name__=="__main__":
     # データ生成
     train, test = get_dataset.load_cifar10()
     train, test = get_dataset.get_specific_label_dataset(class_label_list, train, test)
-    # パラメータの記録
-    log.save_param(args, len(class_label_list), (train,test))
     # 訓練
     tr.train(args, net, train, test)
+    # パラメータの記録
+    log.save_param(args, net.__class__.__name__, class_label_list, (train,test))
--- prototype/model/chainer_cifar10.py
+++ prototype/model/chainer_cifar10.py
@@ -2,10 +2,10 @@ import chainer
 import chainer.links as L
 import chainer.functions as F
 
-class Cifar10(chainer.Chain):
+class Cifar10_Conv6(chainer.Chain):
 
     def __init__(self, n_out=3):
-        super(Cifar10, self).__init__()
+        super(Cifar10_Conv6, self).__init__()
         with self.init_scope():
             self.conv1 = L.Convolution2D(None, 32, 3, 3, 1)
             self.conv2 = L.Convolution2D(32, 32, 3, 3, 1)
@@ -23,6 +23,46 @@ class Cifar10(chainer.Chain):
         h = F.max_pooling_2d(F.relu(self.conv4(h)), 2)
         h = F.relu(self.conv5(h))
         h = F.max_pooling_2d(F.relu(self.conv6(h)), 2)
+        h = F.dropout(F.relu(self.fc1(h)))
+        h = self.fc2(h)
+        return h
+
+
+class Cifar10_Conv4(chainer.Chain):
+
+    def __init__(self, n_out=3):
+        super(Cifar10_Conv4, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(None, 32, 3, 3, 1)
+            self.conv2 = L.Convolution2D(32, 32, 3, 3, 1)
+            self.conv3 = L.Convolution2D(32, 32, 3, 3, 1)
+            self.conv4 = L.Convolution2D(32, 32, 3, 3, 1)
+            self.fc1 = L.Linear(None, 512)
+            self.fc2 = L.Linear(512, n_out)
+
+    def __call__(self, x):
+        h = F.relu(self.conv1(x))
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
+        h = F.relu(self.conv3(h))
+        h = F.max_pooling_2d(F.relu(self.conv4(h)), 2)
+        h = F.dropout(F.relu(self.fc1(h)))
+        h = self.fc2(h)
+        return h
+
+
+class Cifar10_Conv2(chainer.Chain):
+
+    def __init__(self, n_out=3):
+        super(Cifar10_Conv2, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(None, 32, 3, 3, 1)
+            self.conv2 = L.Convolution2D(32, 32, 3, 3, 1)
+            self.fc1 = L.Linear(None, 512)
+            self.fc2 = L.Linear(512, n_out)
+
+    def __call__(self, x):
+        h = F.relu(self.conv1(x))
+        h = F.max_pooling_2d(F.relu(self.conv2(h)), 2)
         h = F.dropout(F.relu(self.fc1(h)))
         h = self.fc2(h)
         return h
