5de71fef41fb20050746f73a52f53edb07f9f46d
oota <shark747474@gmail.com>
2020-04-03 03:45:22 +0900

    add choice_train_at_random

--- prototype/get_dataset.py
+++ prototype/get_dataset.py
@@ -1,4 +1,5 @@
 import chainer
+import random
 
 # cifar10の読み込み
 def load_cifar10():
@@ -33,13 +34,21 @@ def change_label_dataset(new_label, train, test):
     test = change_label_data(new_label, test)
     return train, test
 
+# 指定データ数分のtrainデータをランダムに取得
+# data_n: 1クラスのデータ数
+def choice_train_at_random(data_n, train):
+    train = random.sample(train, data_n)
+    return train
+
 # 指定クラスラベルリストのtrain, testデータセットを取得
-def get_specific_label_dataset(class_label_list, train, test):
+def get_specific_label_dataset(class_label_list, train, test, data_n=None):
     new_train = []
     new_test = []
     for i, label in enumerate(class_label_list):
         got_train, got_test = get_one_label_dataset(label, train, test)
         got_train, got_test = change_label_dataset(i, got_train, got_test)
+        if data_n != None:
+            got_train = choice_train_at_random(data_n, got_train)
         new_train += got_train
         new_test += got_test
     return new_train, new_test
--- prototype/main.py
+++ prototype/main.py
@@ -8,7 +8,7 @@ import sys
 # 学習対象のネットワークを指定
 net = chainer_cifar10.Cifar10_Conv6()
 # 使用するデータのクラスを指定
-class_label_list = [0,1,2]
+class_label_list = [1,2,8]
 
 # モデル名の確認
 def check_model_name(model_name):
@@ -29,7 +29,7 @@ if __name__=="__main__":
     check_model_name(args.model_name)
     # データ生成
     train, test = get_dataset.load_cifar10()
-    train, test = get_dataset.get_specific_label_dataset(class_label_list, train, test)
+    train, test = get_dataset.get_specific_label_dataset(class_label_list, train, test, args.data_n)
     # 訓練
     tr.train(args, net, train, test)
     # パラメータの記録
