5297f571754f0fe731e6d2c34bfbf215ceb8ba92
oota <shark747474@gmail.com>
2020-04-13 22:12:59 +0900

    udpate get_dataset

--- prototype/pytorch_prototype/get_dataset.py
+++ prototype/pytorch_prototype/get_dataset.py
@@ -1,12 +1,8 @@
-import chainer
 import random
 
-# cifar10の読み込み
-def load_cifar10():
-    (train, test) = chainer.datasets.get_cifar10()
-    return train, test
-
-# 複数クラスラベルのデータセットから指定クラスラベルの1データセットを取得
+# データセットからclass_labelのデータセットを取得
+# ex. class_label=1, dataset=[(data, 2),(data, 3),(data, 0) ...]
+# get_one_label_dataset で利用
 def get_one_label_data(class_label, dataset):
     new_data = []
     for data in dataset:
@@ -15,32 +11,40 @@ def get_one_label_data(class_label, dataset):
             new_data.append(data)
     return new_data
 
-# 複数クラスラベルのデータセットから指定クラスラベルのtrain, testデータセットを取得
+# train, testの両データセットからclass_labelのデータセットを取得
+# ex. class_label=1, train=[(data, 2),(data, 3),(data, 0) ...], test=[(data, 2),(data, 3),(data, 0) ...]
+# get_specific_label_dataset で利用
 def get_one_label_dataset(class_label, train, test):
     train = get_one_label_data(class_label, train)
     test = get_one_label_data(class_label, test)
     return train, test
 
-# 1データセットのラベルを指定のラベルに変更
+# データセットのラベルをnew_labelに変更
+# ex. new_label=1, dataset=[(data, 2),(data, 3),(data, 0) ...]
+# change_label_dataset で利用
 def change_label_data(new_label, dataset):
     new_data = []
     for data in dataset:
         new_data.append((data[0], new_label))
     return new_data
 
-# train, testデータセットのラベルを指定のラベルに変更
+# train, testの両データセットのラベルをnew_labelに変更
+# ex. new_label=1, train=[(data, 2),(data, 3),(data, 0) ...], test=[(data, 2),(data, 3),(data, 0) ...]
+# get_specific_label_dataset で利用
 def change_label_dataset(new_label, train, test):
     train = change_label_data(new_label, train)
     test = change_label_data(new_label, test)
     return train, test
 
-# 指定データ数分のtrainデータをランダムに取得
-# data_n: 1クラスのデータ数
-def choice_train_at_random(data_n, train):
-    train = random.sample(train, data_n)
-    return train
+# data_n(1クラスのデータ数)分のデータをランダムに取得
+# ex. data_n=50, dataset=[(data, 2),(data, 3),(data, 0) ...]
+# get_specific_label_dataset で利用
+def choice_data_at_random(data_n, dataset):
+    dataset = random.sample(dataset, data_n)
+    return dataset
 
-# 指定クラスラベルリストのtrain, testデータセットを取得
+# train, testの両データセットからclass_label_listのデータセットを取得
+# ex. class_label_list=[3,4,5], train=[(data, 2),(data, 3),(data, 0) ...], test=[(data, 2),(data, 3),(data, 0) ...], data_n=50
 def get_specific_label_dataset(class_label_list, train, test, data_n=None):
     new_train = []
     new_test = []
@@ -48,7 +52,7 @@ def get_specific_label_dataset(class_label_list, train, test, data_n=None):
         got_train, got_test = get_one_label_dataset(label, train, test)
         got_train, got_test = change_label_dataset(i, got_train, got_test)
         if data_n != None:
-            got_train = choice_train_at_random(data_n, got_train)
+            got_train = choice_data_at_random(data_n, got_train)
         new_train += got_train
         new_test += got_test
     return new_train, new_test
