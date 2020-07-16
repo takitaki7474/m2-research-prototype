55186909ab9976e6e94519ebba8aeb54b61d75a2
oota <shark747474@gmail.com>
2020-07-17 02:33:38 +0900

    Add log_setting function

--- prototype/paramlogging.py
+++ prototype/paramlogging.py
@@ -1,6 +1,20 @@
 import os
 import json
 
+def log_setting(args, net, classes, save_path):
+    setting = {
+    "model": args.model_name,
+    "net": net.__class__.__name__,
+    "epoch": args.epoch,
+    "batch_size": args.batch_size,
+    "initial_lr": args.lr,
+    "classes": classes,
+    "train_num": args.train_num,
+    "test_num": args.test_num,
+    }
+    with open(save_path, "w") as f:
+        json.dump(setting, f, indent=4)
+
 class ParamLogging:
 
     def __init__(self):
