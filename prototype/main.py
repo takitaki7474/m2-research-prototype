7f84a3b5296f845026d9e0f87b43967b8b51b088
oota <shark747474@gmail.com>
2020-07-16 22:03:53 +0900

    Add get_args function to main

---
+++ prototype/main.py
@@ -0,0 +1,15 @@
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', '-mn', default='v1', type=str, help='Name of model to save')
+    parser.add_argument('--train_num', '-trnum', default=None, type=int, help='Number of training data')
+    parser.add_argument('--test_num', '-tenum', default=None, type=int, help='Number of test data')
+    parser.add_argument('--epoch', '-e', type=int, default=100, help='Number of epoch')
+    parser.add_argument('--batch_size', '-b', type=int, default=128, help='Batch size')
+    parser.add_argument('--lr', '-lr', type=float, default=0.001, help='Initial learning rate')
+    args = parser.parse_args()
+    return args
+
+if __name__=="__main__":
+    
