2b21202750f17b7161203884518bd1ac6dc704a6
oota <shark747474@gmail.com>
2020-08-06 00:33:36 +0900

    Add save_feature_table()

--- prototype/feature_extraction.py
+++ prototype/feature_extraction.py
@@ -4,11 +4,14 @@ import pandas as pd
 import json
 import torch
 
-def save_feature_table(df, dbname):
-    pass
+def save_feature_table(df, dbname, tablename):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    df.to_sql(tablename, conn, if_exists='replace')
+    conn.close()
 
-def make_feature_table(net, model_path, data, batch_size):
-    dataloader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False, num_workers=2)
+def make_feature_table(net, model_path, dataset, batch_size):
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=2)
     net.load_state_dict(torch.load(model_path))
     d = defaultdict(list)
     for i, (inputs, labels) in enumerate(dataloader):
