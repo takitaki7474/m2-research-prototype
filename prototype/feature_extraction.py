95324fe3372db55f70a45cec62bb38c6a1415d0c
oota <shark747474@gmail.com>
2020-08-06 00:47:01 +0900

    Clean up

--- prototype/feature_extraction.py
+++ prototype/feature_extraction.py
@@ -1,14 +1,8 @@
 from collections import defaultdict
-import sqlite3
-import pandas as pd
 import json
+import pandas as pd
+import sqlite3
 import torch
-
-def save_feature_table(df, dbname, tablename):
-    conn = sqlite3.connect(dbname)
-    c = conn.cursor()
-    df.to_sql(tablename, conn, if_exists='replace')
-    conn.close()
 
 def make_feature_table(net, model_path, dataset, batch_size):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=2)
@@ -24,3 +18,9 @@ def make_feature_table(net, model_path, dataset, batch_size):
         d[k] = pd.Series(v)
     df = pd.DataFrame(d)
     return df
+
+def save_feature_table(df, dbname, tablename):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    df.to_sql(tablename, conn, if_exists='replace')
+    conn.close()
