885d4ea2054d068ba227f2c2fe824b1797ae1e6b
oota <shark747474@gmail.com>
2020-08-06 00:25:45 +0900

    Add make_feature_table()

--- prototype/feature_extraction.py
+++ prototype/feature_extraction.py
@@ -1,28 +1,23 @@
-import json
+from collections import defaultdict
 import sqlite3
+import pandas as pd
+import json
 import torch
 
-batch_size = 128
-dataloader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False, num_workers=2)
-model_path = "./v3.pth"
-net = PreLeNet(3)
-net.load_state_dict(torch.load(model_path))
-dbname = "features.db"
-conn = sqlite3.connect(dbname)
-c = conn.cursor()
-c.execute("create table featuretable (id integer PRIMARY KEY, label integer, image text, feature text)")
-sql = "insert into featuretable (id, label, image, feature) values (?, ?, ?, ?)"
+def save_feature_table(df, dbname):
+    pass
 
-d = {}
-for i, (inputs, labels) in enumerate(dataloader):
-    outputs, features = net(inputs)
-    for label, image, feature in zip(labels, inputs, features):
-        label = int(label)
-        image = json.dumps(image.cpu().numpy().tolist())
-        feature = json.dumps(feature.data.cpu().numpy().tolist())
-        data = (id, label, image, feature)
-        c.execute(sql, data)
-        id += 1
-
-conn.commit()
-conn.close()
+def make_feature_table(net, model_path, data, batch_size):
+    dataloader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False, num_workers=2)
+    net.load_state_dict(torch.load(model_path))
+    d = defaultdict(list)
+    for i, (inputs, labels) in enumerate(dataloader):
+        outputs, features = net(inputs)
+        for label, image, feature in zip(labels, inputs, features):
+            d["label"].append(int(label))
+            d["image"].append(json.dumps(image.cpu().numpy().tolist()))
+            d["feature"].append(json.dumps(feature.data.cpu().numpy().tolist()))
+    for k, v in d.items():
+        d[k] = pd.Series(v)
+    df = pd.DataFrame(d)
+    return df
