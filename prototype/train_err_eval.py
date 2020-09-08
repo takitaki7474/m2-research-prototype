158276b0c2f9c66eb794dfc3cb0503f0576c591e
oota <shark747474@gmail.com>
2020-09-08 23:22:31 +0900

    Add get_standard_losses function

--- prototype/train_err_eval.py
+++ prototype/train_err_eval.py
@@ -10,3 +10,10 @@ def get_train_losses(train_log):
     for i in train_log:
         train_losses.append(i["train loss"])
     return train_losses
+
+def get_standard_losses(epoch=80):
+    x = list(range(1, epoch+1))
+    standard_losses = []
+    for i in x:
+        standard_losses.append(0.8**i + 0.25)
+    return standard_losses
