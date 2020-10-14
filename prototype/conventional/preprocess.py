0fa0c174f7881de5bc48382b968c95ba3134257c
oota <shark747474@gmail.com>
2020-10-15 01:35:43 +0900

    Fix typo

--- prototype/conventional/preprocess.py
+++ prototype/conventional/preprocess.py
@@ -5,7 +5,7 @@ TRAIN_CLASSES = [1,2,8]
 
 if __name__=="__main__":
 
-    train, test = pre.downloads_cifar10(DATA_PATH)
+    train, test = pre.download_cifar10(DATA_PATH)
 
     preprocessor = pre.DatasetPreprocessor(train, test)
     preprocessor.select_by_label(TRAIN_CLASSES)
