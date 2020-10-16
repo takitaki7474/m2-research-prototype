589df1237988f9460722ce2c353b0f535a33b963
oota <shark747474@gmail.com>
2020-10-17 01:36:00 +0900

    Change settings

--- prototype/conventional/pipeline.sh
+++ prototype/conventional/pipeline.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-LOOPS=2
-MODEL_NAMES=("v1" "v2")
-DT_INDEXES_VERS=("None" "v1")
-TRAINS=(10 10)
-TESTS=(10 10)
-EPOCHS=10
+LOOPS=8
+MODEL_NAMES=("v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16")
+DT_INDEXES_VERS=("None" "v9" "v10" "v11" "v12" "v13" "v14" "v15")
+INI_TRAIN_NUM=300
+ADD_TRAIN_NUM=300
+INI_TEST_NUM=30
+ADD_TEST_NUM=30
+EPOCHS=200
 
 for ((i=0; i<LOOPS; i++))
 do
   echo -e "\nstating ..."
   if [ $i -eq 0 ]
   then
-    params="-mn ${MODEL_NAMES[$i]} -tr ${TRAINS[$i]} -te ${TESTS[$i]} -e $EPOCHS"
+    params="-mn ${MODEL_NAMES[$i]} -tr ${INI_TRAIN_NUM} -te ${INI_TEST_NUM} -e $EPOCHS"
     echo ${params}
     python main.py ${params}
   else
-    params="-mn ${MODEL_NAMES[$i]} -dt ${DT_INDEXES_VERS[$i]} -tr ${TRAINS[$i]} -te ${TESTS[$i]} -e $EPOCHS"
+    params="-mn ${MODEL_NAMES[$i]} -dt ${DT_INDEXES_VERS[$i]} -tr ${ADD_TRAIN_NUM} -te ${ADD_TEST_NUM} -e $EPOCHS"
     echo ${params}
     python main.py ${params}
   fi
