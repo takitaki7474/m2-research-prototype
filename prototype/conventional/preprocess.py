ae19dcf186fe4eb24931f26c772ede418e2ae7af
oota <shark747474@gmail.com>
2020-10-23 02:43:44 +0900

    Use global variable for path variable

--- prototype/conventional/preprocess.py
+++ prototype/conventional/preprocess.py
@@ -1,17 +1,34 @@
+import configparser
 from modules import preprocessor as pre
 
-DATA_PATH = "./data/"
+
+
+inifile = configparser.SafeConfigParser()
+inifile.read("./settings.ini")
+
 TRAIN_CLASSES = [1,2,8]
+
+# 実験データの保存ディレクトリ設定
+DATA_DIR = inifile.get("InputDataDir", "data_dir")
+
+# データセットテーブルの保存ディレクトリ設定
+TRAIN_DATASET_TABLE_PATH = "./dataset_table/train_dt.db"
+TEST_DATASET_TABLE_PATH = "./dataset_table/test_dt.db"
+
+
 
 if __name__=="__main__":
 
-    train, test = pre.download_cifar10(DATA_PATH)
+    # CIFAR10データセットの読み込み
+    train, test = pre.download_cifar10(DATA_DIR)
 
+    # データの前処理 (学習対象ラベルの選択, ラベルを連番に更新, データセットをDataFrame形式に変換)
     preprocessor = pre.DatasetPreprocessor(train, test)
     preprocessor.select_by_label(TRAIN_CLASSES)
     preprocessor.update_labels()
     preprocessor.show_labels()
     train_df, test_df = preprocessor.out_datasets(dataframe=True)
 
-    pre.save(train_df, savepath="./dataset_table/train_dt.db")
-    pre.save(test_df, savepath="./dataset_table/test_dt.db")
+    # DataFrame形式のデータセットの保存
+    pre.save(train_df, savepath=TRAIN_DATASET_TABLE_PATH)
+    pre.save(test_df, savepath=TEST_DATASET_TABLE_PATH)
