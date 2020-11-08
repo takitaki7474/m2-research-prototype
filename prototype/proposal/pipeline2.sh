3a60f1bae0e171d717d8754a22aeb60771d43fe2
oota <shark747474@gmail.com>
2020-11-08 23:11:05 +0900

    Add pipeline v2

---
+++ prototype/proposal/pipeline2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+LOOPS=20
+MODEL_VERS=("v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19" "v20")
+BASE_RESULT_VERS=("None" "v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19")
+INI_TRAIN_NUM=8
+ADD_TRAIN_NUM=8
+INI_TEST_NUM=2
+ADD_TEST_NUM=2
+LEARNING_RATE=0.01
+SEED=0
+EPOCHS=100
+
+for ((i=0; i<LOOPS; i++))
+do
+  echo -e "\nstating ..."
+
+
+  if [ $i -eq 0 ]; then
+    params="-mn ${MODEL_VERS[$i]} -tr $INI_TRAIN_NUM -te $INI_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED -eval 0"
+    echo ${params}
+    python re_learning.py ${params}
+  else
+    #eval_result=$(python comp_loss.py -mv1 ${BASE_RESULT_VERS[$i]})
+    eval_result=1
+    params="-mn ${MODEL_VERS[$i]} -bv ${BASE_RESULT_VERS[$i]} -tr $ADD_TRAIN_NUM -te $ADD_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED -eval $eval_result"
+    echo ${params}
+    python re_learning.py ${params}
+  fi
+
+
+  echo -e "\n"
+done
