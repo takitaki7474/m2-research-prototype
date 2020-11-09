d0d3828b2f0972deb10fc7b58d81bd47c61aa34e
oota <shark747474@gmail.com>
2020-11-09 22:22:54 +0900

    Change settings to classify 3 classes

--- prototype/proposal/pipeline.sh
+++ prototype/proposal/pipeline.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 LOOPS=20
 MODEL_VERS=("v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19" "v20")
 BASE_RESULT_VERS=("None" "v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19")
@@ -8,8 +9,9 @@ ADD_TRAIN_NUM=40
 INI_TEST_NUM=10
 ADD_TEST_NUM=10
 LEARNING_RATE=0.01
-SEED=4
+SEED=8
 EPOCHS=100
+
 
 eval_result=0
 for ((i=0; i<LOOPS; i++))
@@ -19,14 +21,10 @@ do
 
   if [ $i -eq 0 ]; then
     params="-mn ${MODEL_VERS[$i]} -tr $INI_TRAIN_NUM -te $INI_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED"
-    echo ${params}
-    python re_learning.py ${params}
 
 
   elif [ $i -eq 1 ]; then
     params="-mn ${MODEL_VERS[$i]} -bv ${BASE_RESULT_VERS[$i]} -tr $ADD_TRAIN_NUM -te $ADD_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED"
-    echo ${params}
-    python re_learning.py ${params}
 
 
   else
@@ -36,10 +34,10 @@ do
       eval_result=$(python comp_loss.py -mv1 ${BASE_RESULT_VERS[`expr $i - 1`]} -mv2 ${MODEL_VERS[`expr $i - 1`]})
     fi
     params="-mn ${MODEL_VERS[$i]} -bv ${BASE_RESULT_VERS[$i]} -tr $ADD_TRAIN_NUM -te $ADD_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED -eval $eval_result"
-    echo ${params}
-    python re_learning.py ${params}
   fi
 
 
+  echo ${params}
+  python re_learning.py ${params}
   echo -e "\n"
 done
--- prototype/proposal/pipeline2.sh
+++ prototype/proposal/pipeline2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-LOOPS=20
-MODEL_VERS=("v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19" "v20")
-BASE_RESULT_VERS=("None" "v1" "v2" "v3" "v4" "v5" "v6" "v7" "v8" "v9" "v10" "v11" "v12" "v13" "v14" "v15" "v16" "v17" "v18" "v19")
-INI_TRAIN_NUM=8
-ADD_TRAIN_NUM=8
-INI_TEST_NUM=2
-ADD_TEST_NUM=2
+LOOPS=5
+MODEL_VERS=("v21" "v22" "v23" "v24" "v25")
+BASE_RESULT_VERS=("v20" "v21" "v22" "v23" "v24")
+INI_TRAIN_NUM=40
+ADD_TRAIN_NUM=40
+INI_TEST_NUM=10
+ADD_TEST_NUM=10
 LEARNING_RATE=0.01
 SEED=0
 EPOCHS=100
@@ -18,16 +18,13 @@ do
 
   if [ $i -eq 0 ]; then
     params="-mn ${MODEL_VERS[$i]} -tr $INI_TRAIN_NUM -te $INI_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED -eval 0"
-    echo ${params}
-    python re_learning.py ${params}
   else
-    #eval_result=$(python comp_loss.py -mv1 ${BASE_RESULT_VERS[$i]})
-    eval_result=1
+    eval_result=$(python comp_loss.py -mv1 ${BASE_RESULT_VERS[$i]})
     params="-mn ${MODEL_VERS[$i]} -bv ${BASE_RESULT_VERS[$i]} -tr $ADD_TRAIN_NUM -te $ADD_TEST_NUM -e $EPOCHS -lr $LEARNING_RATE -s $SEED -eval $eval_result"
-    echo ${params}
-    python re_learning.py ${params}
   fi
 
-
+  
+  echo ${params}
+  python re_learning.py ${params}
   echo -e "\n"
 done
