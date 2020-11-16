1e7c3ee5da22ed2510c85609da601d7b6d2d7dc7
oota <shark747474@gmail.com>
2020-11-17 01:00:15 +0900

    Update re_learning process

--- prototype/proposal/re_learning.py
+++ prototype/proposal/re_learning.py
@@ -9,7 +9,7 @@ import torch
 from model import domain_models
 from modules import argument, dataselector, training, plot, eval_loss
 from modules import preprocessor as pre
-from utils import alert, tools, lr_patterns
+from utils import alert, tools, lr_patterns, handle_dt_indexes
 
 
 
@@ -21,7 +21,11 @@ inifile = configparser.SafeConfigParser()
 inifile.read("./settings.ini")
 
 # 学習モデルの設定
-MODEL = domain_models.LeNet3(3)
+CLASS_NUM = 3
+MODEL = domain_models.LeNet3(CLASS_NUM)
+
+# クエリ数の設定
+QUERY_NUM = 1
 
 # データセットテーブルの読み込みディレクトリ設定
 FEATURE_TABLE_PATH = "./feature_table/ft.db"
@@ -73,29 +77,23 @@ if __name__=="__main__":
     # 学習結果の保存ディレクトリを生成
     if not os.path.isdir(LEARN_RESULT_DIR): os.mkdir(LEARN_RESULT_DIR)
 
-    """
-    # 前回学習の訓練誤差収束速度の評価
-    if args.base_result_ver is not None:
-        err_speed = eval_loss.load(path=ERR_SPEED_LOAD_PATH)
-        if err_speed >= args.err_speed_std: # 訓練誤差収束速度が基準値を満たした場合
-            ERR_SPEED_EVAL = 1
-    ERR_SPEED_EVAL = 1 # 実験用
-    """
-
 
     # 訓練データの選択と生成
     train_ft = pre.load(dbpath=FEATURE_TABLE_PATH)
     if args.base_result_ver is None:
-        train_ft_indexes = dataselector.init_feature_table_indexes(feature_table=train_ft)
+        train_ft_indexes = handle_dt_indexes.init_ft_indexes(ft=train_ft, queryN=QUERY_NUM, seed=args.seed)
     else:
-        train_ft_indexes = dataselector.load_table_indexes(path=BASE_TRAIN_FT_INDEXES_PATH)
+        train_ft_indexes = handle_dt_indexes.load_dt_indexes(path=BASE_TRAIN_FT_INDEXES_PATH)
     selector = dataselector.FeatureSelector(train_ft, train_ft_indexes)
-    selector.make_faiss_indexes()
     if ERR_SPEED_EVAL == 1: # 訓練誤差収束速度が基準を満たした場合
-        _ = selector.update_FP_queries() # クエリを最遠傍点(FP)に更新
-    train_ft_indexes, _ = selector.add_NN(dataN=args.add_train)
-    train = selector.out_selected_dataset()
-    dataselector.save_feature_table_indexes(train_ft_indexes, savepath=ADDED_TRAIN_FT_INDEXES_PATH)
+        _ = selector.update_to_FP_queries() # クエリを最遠傍点(FP)に更新
+    dataN_queryby = args.add_train // QUERY_NUM
+    dataN_randomly = args.add_train % QUERY_NUM
+    _ = selector.select_NN_ft_indexes(dataN=dataN_queryby)
+    if dataN_randomly != 0:
+        _ = selector.randomly_select_dt_indexes(dataN=dataN_randomly, seed=args.seed)
+    train = selector.get_dataset()
+    handle_dt_indexes.save_dt_indexes(train_ft_indexes, savepath=ADDED_TRAIN_FT_INDEXES_PATH)
     print("Number of train data: {0}".format(len(train)))
     trainloader = torch.utils.data.DataLoader(train, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
@@ -103,13 +101,13 @@ if __name__=="__main__":
     # テストデータの選択と生成
     test_dt = pre.load(dbpath=TEST_DATASET_TABLE_PATH)
     if args.base_result_ver is None:
-        test_dt_indexes = dataselector.init_dataset_table_indexes()
+        test_dt_indexes = handle_dt_indexes.init_dt_indexes(dt=test_dt)
     else:
-        test_dt_indexes = dataselector.load_table_indexes(path=BASE_TEST_DT_INDEXES_PATH)
+        test_dt_indexes = handle_dt_indexes.load_dt_indexes(path=BASE_TEST_DT_INDEXES_PATH)
     selector = dataselector.DataSelector(test_dt, test_dt_indexes)
-    test_dt_indexes, _ = selector.randomly_add(dataN=args.add_test, seed=args.seed)
-    test = selector.out_selected_dataset()
-    dataselector.save_dataset_table_indexes(test_dt_indexes, savepath=ADDED_TEST_DT_INDEXES_PATH)
+    _ = selector.randomly_select_dt_indexes(dataN=args.add_test, seed=args.seed)
+    test = selector.get_dataset()
+    handle_dt_indexes.save_dt_indexes(test_dt_indexes, savepath=ADDED_TEST_DT_INDEXES_PATH)
     print("Number of test data: {0}".format(len(test)))
     testloader = torch.utils.data.DataLoader(test, batch_size=args.test_batch_size, shuffle=False, num_workers=2)
 
